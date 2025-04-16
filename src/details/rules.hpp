10a591c2c0141b54727174e1ecc2658b81ff4957
Dmitriy <Dmitry5335@yandex.ru>
2025-04-17 01:39:12 +0300

    feat: add MovingByExtRule

--- src/details/rules.hpp
+++ src/details/rules.hpp
@@ -2,11 +2,26 @@
 #define RULES_HPP
 
 #include <filesystem>
+#include <vector>
+#include <set>
 #include "../additionals/json.hpp"
+
+using path = std::filesystem::path;
+using paths = std::vector< std::filesystem::path >;
+using set = std::set< path >;
 
 namespace details
 {
-  
+  struct MovingByExtRule
+  {
+    template< class PathT, class ExtensionsT, class ExceptionsT >
+    MovingByExtRule(PathT&& targetDir, ExtensionsT&& ext, ExceptionsT&& exceptions);
+    ~MovingByExtRule() = default;
+
+    path targetDir;
+    paths ext;
+    set exceptions;
+  };
 }
 
 #endif
