c478be1894752d7000c969ad51987b24a93d5229
Dmitriy <Dmitry5335@yandex.ru>
2025-04-17 01:51:41 +0300

    feat: add Dir class declaration for methods

--- src/details/dir.hpp
+++ src/details/dir.hpp
@@ -6,14 +6,24 @@
 #include "../additionals/json.hpp"
 #include "rules.hpp"
 
+using json = nlohmann::json;
+
 namespace details
 {
   class Dir
   {
-  public:
+    public:
     Dir() = default;
     Dir(const std::vector< FileRule >& rules);
     ~Dir() = default;
+
+    template < class T, class... Args >
+    void addRule(Args... args);
+
+    void deleteRule(size_t id);
+    const std::vector< FileRule >& getRules() const;
+
+    json toJson() const;
 
   private:
     std::vector< FileRule > rules_;
