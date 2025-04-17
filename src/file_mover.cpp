40fd2bd30243d66e12241e64e15eeaae49e50111
Dmitriy <Dmitry5335@yandex.ru>
2025-04-17 16:31:27 +0300

    feat: add methods realisation for rule adding

--- src/file_mover.cpp
+++ src/file_mover.cpp
@@ -9,6 +9,44 @@ void FileMover::addDir(fs::path dirName)
   }
 }
 
+void FileMover::addRuleToDir(fs::path dirName, fs::path targetDir, paths ext, set exceptions)
+{
+  set relatieveExceptions = makeRelatieve(dirName, exceptions);
+  addRule< details::MovingByExtRule >(dirName, targetDir, ext, relatieveExceptions);
+}
+
+void FileMover::addRuleToDir(fs::path dirName, fs::path targetDir, days duration, bool isGreaterThanDuration,
+  set exceptions)
+{
+  set relatieveExceptions = makeRelatieve(dirName, exceptions);
+  addRule< details::MovingByDateRule >(dirName, targetDir, duration, isGreaterThanDuration, relatieveExceptions);
+}
+
+void FileMover::addRuleToDir(fs::path dirName, fs::path targetDir, std::string name, bool isCheckRegister,
+  set exceptions)
+{
+  set relatieveExceptions = makeRelatieve(dirName, exceptions);
+  addRule< details::MovingByNameRule >(dirName, targetDir, name, isCheckRegister, relatieveExceptions);
+}
+
+void FileMover::addRuleToDir(fs::path dirName, fs::path targetDir, set exceptions)
+{
+  set relatieveExceptions = makeRelatieve(dirName, exceptions);
+  addRule< details::MovingAllRule >(dirName, targetDir, relatieveExceptions);
+}
+
+void FileMover::addRuleToDir(fs::path dirName, paths ext, set exceptions)
+{
+  set relatieveExceptions = makeRelatieve(dirName, exceptions);
+  addRule< details::DeletingByExtRule >(dirName, ext, relatieveExceptions);
+}
+
+void FileMover::addRuleToDir(fs::path dirName, days duration, bool isGreaterThanDuration, set exceptions)
+{
+  set relatieveExceptions = makeRelatieve(dirName, exceptions);
+  addRule< details::DeletingByDateRule >(dirName, duration, isGreaterThanDuration, relatieveExceptions);
+}
+
 set FileMover::makeRelatieve(fs::path dirName, const set& exceptions) const
 {
   set relatieveExceptions;
