4115a7b792a10cc6482d33a6a8b61b419c7998d3
Dmitriy <Dmitry5335@yandex.ru>
2025-04-17 16:10:45 +0300

    feat: add delete rule method definition

--- src/details/dir.cpp
+++ src/details/dir.cpp
@@ -5,3 +5,17 @@ using namespace details;
 Dir::Dir(const std::vector< FileRule >& rules):
   rules_(rules)
 {}
+
+void Dir::deleteRule(size_t index)
+{
+  if (index < rules_.size())
+  {
+    auto iterator = rules_.cbegin();
+    std::advance(iterator, index);
+    rules_.erase(iterator);
+  }
+  else
+  {
+    throw std::out_of_range("Error: Index is out of range");
+  }
+}
