44a6646e471b3b90df0f4a76dac74b333803af43
Dmitriy <Dmitry5335@yandex.ru>
2025-04-17 16:23:33 +0300

    feat: add declaration for rule adding and deleting from dirs

--- src/file_mover.hpp
+++ src/file_mover.hpp
@@ -3,6 +3,7 @@
 
 #include <map>
 #include <filesystem>
+#include <fstream>
 #include "details/dir.hpp"
 
 namespace fs = std::filesystem;
@@ -12,6 +13,18 @@ class FileMover
 public:
   FileMover() = default;
   ~FileMover() = default;
+
+  void addDir(fs::path dirName);
+
+  void addRuleToDir(fs::path dirName, fs::path targetDir, paths ext, set exceptions);
+  void addRuleToDir(fs::path dirName, fs::path targetDir, days duration, bool isGreaterThanDuration, set exceptions);
+  void addRuleToDir(fs::path dirName, fs::path targetDir, std::string name, bool isCheckRegister, set exceptions);
+  void addRuleToDir(fs::path dirName, fs::path targetDir, set exceptions);
+  void addRuleToDir(fs::path dirName, paths ext, set exceptions);
+  void addRuleToDir(fs::path dirName, days duration, bool isGreaterThanDuration, set exceptions);
+
+  void deleteRuleFromDir(fs::path dirName, size_t id);
+  void deleteDir(fs::path dirName);
 
 private:
   std::map< fs::path, details::Dir > dirs_;
