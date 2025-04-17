518d3a128841407567e8d31335db98004f87df73
Dmitriy <Dmitry5335@yandex.ru>
2025-04-17 20:27:16 +0300

    feat: add MovingByExtRule visitor

--- src/details/visitors.cpp
+++ src/details/visitors.cpp
@@ -63,6 +63,38 @@ json JsonVisitor::operator()(const DeletingByDateRule& rule) const
   };
 }
 
+void RuleVisitor::operator()(const MovingByExtRule& rule) const
+{
+  std::vector< path > files;
+  auto options = fs::directory_options::skip_permission_denied;
+  for (auto i = fs::recursive_directory_iterator(dirPath_, options); i != fs::recursive_directory_iterator(); ++i)
+  {
+    bool shouldIgnore = checkFile(fs::relative((*i), dirPath_), rule.exceptions)
+      || fs::equivalent(*i, rule.targetDir);
+
+    if (shouldIgnore && fs::is_directory(*i))
+    {
+      i.disable_recursion_pending();
+    }
+    else if (!shouldIgnore && fs::is_regular_file(*i))
+    {
+      for (auto j = rule.ext.cbegin(); j != rule.ext.cend(); ++j)
+      {
+        if (fs::equivalent((*i).path().extension(), *j))
+        {
+          files.push_back(*i);
+          break;
+        }
+      }
+    }
+  }
+
+  for (auto i = files.begin(); i != files.end(); ++i)
+  {
+    moveFile(dirPath_ / (*i), rule.targetDir);
+  }
+}
+
 void RuleVisitor::moveFile(const path& oldPath, const path& newPath) const
 {
   if (fs::exists(oldPath) && fs::exists(newPath) && !fs::exists(fs::path(newPath) / fs::path(oldPath).filename()))
