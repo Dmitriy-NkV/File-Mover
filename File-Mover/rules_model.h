f63cb68dcb622a0eb2574cf7cf7765ca65d1c6d5
Dmitriy <Dmitry5335@yandex.ru>
2025-04-22 23:11:41 +0300

    feat: add rules model for more easier parsing

---
+++ File-Mover/rules_model.h
@@ -0,0 +1,36 @@
+#ifndef RULES_MODEL_H
+#define RULES_MODEL_H
+
+#include <QAbstractListModel>
+#include "C:\\Users\\Dmitr\\Documents\\File-Mover\\src\\file_mover.hpp"
+
+class RulesModel: public QAbstractListModel
+{
+  Q_OBJECT
+public:
+  enum RuleRoles
+  {
+    TypeRole = Qt::UserRole + 1,
+    TargetDirRole,
+    ExtensionsRole,
+    DurationRole,
+    NameRole,
+    IsGreaterThanDurationRole,
+    IsCheckRegisterRole,
+    ExceptionsRole
+  };
+
+  explicit RulesModel(FileMover& fileMover, QObject* parent = nullptr);
+
+  Q_INVOKABLE void loadRulesForDir(const QString& dirName);
+
+  int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+  QVariant data(const QModelIndex& index, int role) const override;
+  QHash<int, QByteArray> roleNames() const override;
+
+private:
+  FileMover& fileMover_;
+  std::vector<details::FileRule> rules_;
+};
+
+#endif // RULES_MODEL_H
