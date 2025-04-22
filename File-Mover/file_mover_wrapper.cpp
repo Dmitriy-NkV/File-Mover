7073b4d21d7178aa4b6db4556ad9a933552669fb
Dmitriy <Dmitry5335@yandex.ru>
2025-04-22 22:58:33 +0300

    feat: add wrapper for fileMover class

--- File-Mover/file_mover_wrapper.cpp
+++ File-Mover/file_mover_wrapper.cpp
@@ -1,60 +1,271 @@
 #include "file_mover_wrapper.h"
 
-FileMoverWrapper::FileMoverWrapper(QObject *parent)
-    : QObject{parent}
-{}
-
-void FileMoverWrapper::addDir(std::filesystem::path dirName)
+FileMoverWrapper::FileMoverWrapper(QObject* parent):
+  QObject(parent),
+  rulesModel_(new RulesModel(fileMover_, this))
 {
-    fileMover_.addDir(dirName);
+  connect(&fileWatcher_, &QFileSystemWatcher::directoryChanged, this, [this](const QString& path)
+  {
+    try
+    {
+      fileMover_.execDir(path.toStdString());
+    }
+    catch(const std::exception& e)
+    {
+      qDebug() << e.what() << '\n';
+    }
+  });
 }
 
-void FileMoverWrapper::addRuleToDir(std::filesystem::path dirName, std::filesystem::path targetDir, std::vector< std::filesystem::path > ext, std::set< std::filesystem::path > exceptions)
+void FileMoverWrapper::addDir(const QString& dirName)
 {
-    fileMover_.addRuleToDir(dirName, targetDir, ext, exceptions);
+  try
+  {
+    fileMover_.addDir(dirName.toStdString());
+    emit dirsChanged();
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
 }
 
-void FileMoverWrapper::addRuleToDir(std::filesystem::path dirName, std::filesystem::path targetDir, std::chrono::duration< int, std::ratio< 86400 > > duration, bool isGreaterThanDuration, std::set< std::filesystem::path > exceptions)
+void FileMoverWrapper::addRuleToDir(const QString& dirName, const QString& targetDir, const QStringList& ext, const QStringList& exceptions)
 {
-    fileMover_.addRuleToDir(dirName, targetDir, duration, isGreaterThanDuration, exceptions);
+  std::vector< std::filesystem::path > extVec;
+  for (const auto& e: ext)
+  {
+    extVec.push_back(e.toStdString());
+  }
+
+  std::set< std::filesystem::path > exceptionsSet;
+  for (const auto& ex: exceptions)
+  {
+    exceptionsSet.insert(ex.toStdString());
+  }
+
+  try
+  {
+    fileMover_.addRuleToDir(dirName.toStdString(), targetDir.toStdString(), extVec, exceptionsSet);
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
 }
 
-void FileMoverWrapper::addRuleToDir(std::filesystem::path dirName, std::filesystem::path targetDir, std::string name, bool isCheckRegister, std::set< std::filesystem::path > exceptions)
+void FileMoverWrapper::addRuleToDir(const QString& dirName, const QString& targetDir, int durationDays, bool isGreaterThanDuration, const QStringList& exceptions)
 {
-    fileMover_.addRuleToDir(dirName, targetDir, name, isCheckRegister, exceptions);
+  std::set < std::filesystem::path > exceptionsSet;
+  for (const auto& ex: exceptions)
+  {
+    exceptionsSet.insert(ex.toStdString());
+  }
+
+  auto duration = std::chrono::duration< int, std::ratio< 86400 > >(durationDays);
+
+  try
+  {
+    fileMover_.addRuleToDir(dirName.toStdString(), targetDir.toStdString(), duration, isGreaterThanDuration, exceptionsSet);
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
+
 }
 
-void FileMoverWrapper::addRuleToDir(std::filesystem::path dirName, std::filesystem::path targetDir, std::set< std::filesystem::path > exceptions)
+void FileMoverWrapper::addRuleToDir(const QString& dirName, const QString& targetDir, const QString& name, bool isCheckRegister,const QStringList& exceptions)
 {
-    fileMover_.addRuleToDir(dirName, targetDir, exceptions);
+  std::set< std::filesystem::path > exceptionsSet;
+  for (const auto& ex: exceptions)
+  {
+    exceptionsSet.insert(ex.toStdString());
+  }
+
+  try
+  {
+    fileMover_.addRuleToDir(dirName.toStdString(), targetDir.toStdString(), name.toStdString(), isCheckRegister, exceptionsSet);
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
+
 }
 
-void FileMoverWrapper::addRuleToDir(std::filesystem::path dirName, std::vector< std::filesystem::path > ext, std::set< std::filesystem::path > exceptions)
+void FileMoverWrapper::addRuleToDir(const QString& dirName, const QString& targetDir, const QStringList& exceptions)
 {
-    fileMover_.addRuleToDir(dirName, ext, exceptions);
+  std::set< std::filesystem::path > exceptionsSet;
+  for (const auto& ex: exceptions)
+  {
+    exceptionsSet.insert(ex.toStdString());
+  }
+
+  try
+  {
+    fileMover_.addRuleToDir(dirName.toStdString(), targetDir.toStdString(), exceptionsSet);
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
 }
 
-void FileMoverWrapper::addRuleToDir(std::filesystem::path dirName, std::chrono::duration< int, std::ratio< 86400 > > duration, bool isGreaterThanDuration, std::set< std::filesystem::path > exceptions)
+void FileMoverWrapper::addRuleToDir(const QString& dirName, const QStringList& ext, const QStringList& exceptions)
 {
-    fileMover_.addRuleToDir(dirName, duration, isGreaterThanDuration, exceptions);
+  std::vector< std::filesystem::path > extVec;
+  for (const auto& e: ext)
+  {
+    extVec.push_back(e.toStdString());
+  }
+
+  std::set< std::filesystem::path > exceptionsSet;
+  for (const auto& ex: exceptions)
+  {
+    exceptionsSet.insert(ex.toStdString());
+  }
+
+  try
+  {
+    fileMover_.addRuleToDir(dirName.toStdString(), extVec, exceptionsSet);
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
 }
 
-void FileMoverWrapper::deleteRuleFromDir(std::filesystem::path dirName, size_t id)
+void FileMoverWrapper::addRuleToDir(const QString& dirName, int durationDays, bool isGreaterThanDuration, const QStringList& exceptions)
 {
-    fileMover_.deleteRuleFromDir(dirName, id);
+  std::set< std::filesystem::path > exceptionsSet;
+  for (const auto& ex: exceptions)
+  {
+    exceptionsSet.insert(ex.toStdString());
+  }
+
+  auto duration = std::chrono::duration< int, std::ratio< 86400 > >(durationDays);
+
+  try
+  {
+    fileMover_.addRuleToDir(dirName.toStdString(), duration, isGreaterThanDuration, exceptionsSet);
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
 }
 
-void FileMoverWrapper::deleteDir(std::filesystem::path dirName)
+void FileMoverWrapper::deleteRuleFromDir(const QString& dirName, int id)
 {
-    fileMover_.deleteDir(dirName);
+  try
+  {
+    fileMover_.deleteRuleFromDir(dirName.toStdString(), static_cast< size_t >(id));
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
 }
 
-void FileMoverWrapper::importConfig(std::filesystem::path config)
+void FileMoverWrapper::deleteDir(const QString& dirName)
 {
-    fileMover_.importConfig(config);
+  try
+  {
+    fileMover_.deleteDir(dirName.toStdString());
+    emit dirsChanged();
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
 }
 
-nlohmann::json FileMoverWrapper::saveConfig() const
+Q_INVOKABLE void FileMoverWrapper::clear()
 {
-    return fileMover_.saveConfig();
+  auto dirs = fileMover_.getDirs();
+  for (auto i = dirs.cbegin(); i != dirs.cend(); ++i)
+  {
+    fileMover_.deleteDir(*i);
+  }
+}
+
+void FileMoverWrapper::importConfig(const QString& config)
+{
+  try
+  {
+    fileMover_.importConfig(config.toStdString());
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
+}
+
+void FileMoverWrapper::saveConfig(const QString& config) const
+{
+  try
+  {
+    fileMover_.saveConfig(config.toStdString());
+  }
+  catch(const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
+}
+
+void FileMoverWrapper::startWatching()
+{
+  try
+  {
+    if (!isWatching_ && !getDirs().empty())
+    {
+      fileMover_.execDirs();
+      fileWatcher_.addPaths(getDirs());
+      isWatching_ = true;
+      emit watchingStateChanged();
+    }
+  }
+  catch (const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+    stopWatching();
+  }
+}
+
+void FileMoverWrapper::stopWatching()
+{
+  try
+  {
+    if (isWatching_)
+    {
+      fileWatcher_.removePaths(fileWatcher_.directories());
+      isWatching_ = false;
+      emit watchingStateChanged();
+    }
+  }
+  catch (const std::exception& e)
+  {
+    qDebug() << e.what() << '\n';
+  }
+}
+
+bool FileMoverWrapper::isWatching() const
+{
+  return isWatching_;
+}
+
+QStringList FileMoverWrapper::getDirs() const
+{
+  QStringList list;
+  for (const auto& dir: fileMover_.getDirs())
+  {
+    list << QString::fromStdString(dir.string());
+  }
+  return list;
+}
+
+RulesModel* FileMoverWrapper::rulesModel() const
+{
+  return rulesModel_;
 }
--- File-Mover/file_mover_wrapper.h
+++ File-Mover/file_mover_wrapper.h
@@ -2,36 +2,53 @@
 #define FILE_MOVER_WRAPPER_H
 
 #include <QObject>
+#include <QFileSystemWatcher>
+#include "rules_model.h"
 #include "C:\\Users\\Dmitr\\Documents\\File-Mover\\src\\file_mover.hpp"
 
-class FileMoverWrapper : public QObject
+class FileMoverWrapper: public QObject
 {
-    Q_OBJECT
+  Q_OBJECT
+
+  Q_PROPERTY(QStringList dirs READ getDirs NOTIFY dirsChanged)
+  Q_PROPERTY(RulesModel* rulesModel READ rulesModel CONSTANT)
+  Q_PROPERTY(bool isWatching READ isWatching NOTIFY watchingStateChanged)
+
 public:
-    explicit FileMoverWrapper(QObject *parent = nullptr);
+  explicit FileMoverWrapper(QObject *parent = nullptr);
 
-    Q_INVOKABLE void addDir(std::filesystem::path dirName);
+  Q_INVOKABLE void addDir(const QString& dirName);
 
-    Q_INVOKABLE void addRuleToDir(std::filesystem::path dirName, std::filesystem::path targetDir, std::vector< std::filesystem::path > ext, std::set< std::filesystem::path > exceptions);
-    Q_INVOKABLE void addRuleToDir(std::filesystem::path dirName, std::filesystem::path targetDir, std::chrono::duration< int, std::ratio< 86400 > > duration, bool isGreaterThanDuration, std::set< std::filesystem::path > exceptions);
-    Q_INVOKABLE void addRuleToDir(std::filesystem::path dirName, std::filesystem::path targetDir, std::string name, bool isCheckRegister, std::set< std::filesystem::path > exceptions);
-    Q_INVOKABLE void addRuleToDir(std::filesystem::path dirName, std::filesystem::path targetDir, std::set< std::filesystem::path > exceptions);
-    Q_INVOKABLE void addRuleToDir(std::filesystem::path dirName, std::vector< std::filesystem::path > ext, std::set< std::filesystem::path > exceptions);
-    Q_INVOKABLE void addRuleToDir(std::filesystem::path dirName, std::chrono::duration< int, std::ratio< 86400 > > duration, bool isGreaterThanDuration, std::set< std::filesystem::path > exceptions);
+  Q_INVOKABLE void addRuleToDir(const QString& dirName, const QString& targetDir, const QStringList& ext, const QStringList& exceptions);
+  Q_INVOKABLE void addRuleToDir(const QString& dirName, const QString& targetDir, int durationDays, bool isGreaterThanDuration, const QStringList& exceptions);
+  Q_INVOKABLE void addRuleToDir(const QString& dirName, const QString& targetDir, const QString& name, bool isCheckRegister, const QStringList& exceptions);
+  Q_INVOKABLE void addRuleToDir(const QString& dirName, const QString& targetDir, const QStringList& exceptions);
+  Q_INVOKABLE void addRuleToDir(const QString& dirName, const QStringList& ext, const QStringList& exceptions);
+  Q_INVOKABLE void addRuleToDir(const QString& dirName, int durationDays, bool isGreaterThanDuration, const QStringList& exceptions);
 
-    Q_INVOKABLE void deleteRuleFromDir(std::filesystem::path dirName, size_t id);
-    Q_INVOKABLE void deleteDir(std::filesystem::path dirName);
+  Q_INVOKABLE void deleteRuleFromDir(const QString& dirName, int id);
+  Q_INVOKABLE void deleteDir(const QString& dirName);
+  Q_INVOKABLE void clear();
 
-    Q_INVOKABLE void importConfig(std::filesystem::path config);
-    Q_INVOKABLE nlohmann::json saveConfig() const;
+  Q_INVOKABLE void importConfig(const QString& config);
+  Q_INVOKABLE void saveConfig(const QString& config) const;
 
-// signals:
-//     void valueChanged();
-//     void nameChanged();
-//     void operationCompleted(bool success);
+  Q_INVOKABLE void startWatching();
+  Q_INVOKABLE void stopWatching();
+
+  Q_INVOKABLE bool isWatching() const;
+  Q_INVOKABLE QStringList getDirs() const;
+  Q_INVOKABLE RulesModel* rulesModel() const;
+
+signals:
+  void dirsChanged();
+  void watchingStateChanged();
 
 private:
-    FileMover fileMover_;
+  bool isWatching_ = false;
+  FileMover fileMover_;
+  RulesModel* rulesModel_;
+  QFileSystemWatcher fileWatcher_;
 };
 
 #endif // FILE_MOVER_WRAPPER_H
